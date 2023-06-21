2ea718c3230ed42abf9a1d239740167e711d8020
henhadayo1 <henhadayo@gmail.com>
2023-06-21 11:32:37 +0300

    add sendAddNotificationRequest function to add a new notification while a new todo is added

--- src/controllers/todoController.js
+++ src/controllers/todoController.js
@@ -1,5 +1,6 @@
 import Todo from "../models/Todo.js";
 import asyncHandler from "express-async-handler";
+import axios from "axios";
 
 export const getAllTodos = asyncHandler(async (req, res) => {
   const todos = await Todo.find();
@@ -29,6 +30,7 @@ export const addTodo = asyncHandler(async (req, res) => {
   const todo = await new Todo(req.body);
   await todo.save();
   console.log("Todo item as been saved!", todo);
+  await sendAddNotificationRequest(todo._id, req.body.deadline);
   res.json("Created!");
 });
 
@@ -38,3 +40,22 @@ export const deleteTodo = asyncHandler(async (req, res) => {
   console.log("Delete todo item by id", todo);
   res.json("Deleted!");
 });
+
+const sendAddNotificationRequest = async (todoId, deadline) => {
+  try {
+    console.log(
+      "Sending a request to add a new notification",
+      todoId,
+      deadline
+    );
+    const response = await axios.post(
+      process.env.NOTIFICATION_ENDPOINT ||
+        "http://localhost:4000/notifications",
+      { deadline, todo: todoId }
+    );
+    console.log("A new notification added!", response.data);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
